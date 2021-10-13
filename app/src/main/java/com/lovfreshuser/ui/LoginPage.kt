714218c427b670ad3321bc733bd119830536591e
Rahatul Aseaikin Shovo <rahatshovo6666@gmail.com>
2021-10-13 13:33:13 +0600

    added res

---
+++ app/src/main/java/com/lovfreshuser/ui/LoginPage.kt
@@ -0,0 +1,12 @@
+package com.lovfreshuser.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lovfreshuser.R
+
+class LoginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+    }
+}
---
+++ app/src/main/java/com/lovfreshuser/ui/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.lovfreshuser.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lovfreshuser.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
